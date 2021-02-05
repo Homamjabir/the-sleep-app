1360f5928e36a4e683aff7c962681be3750a76ea
Alexander Palm <alepa@kth.se>
2021-02-05 15:10:13 +0100

    Sign in anonimously

--- src/components/about.js
+++ src/components/about.js
@@ -1,32 +1,40 @@
 import React, { Component } from "react";
-import {Route} from "react-router-dom";
-import "./css/menu.css"
+import { Route } from "react-router-dom";
+import "./css/menu.css";
 
 class About extends Component {
-    render() {
-      return (
-        <div className = "SettingsContainer">
-            <h1>About</h1>
+  render() {
+    return (
+      <div className="SettingsContainer">
+        <h1>About</h1>
 
-            <h3>This app is a tool to help aid Karolinska Institutet<br></br>
-             in their study about sleep patterns and its effects, <br></br>by gathering data from study participants.<br></br>
-               <br></br>
-               Your participation will contribute to<br></br>
-                 researchers gaining more knowledge about<br></br>
-                 sleep patterns, and in turn becoming more able<br></br>
-                  to help people live more productive and fulfilling lives.
-            </h3>
-            <div className = "menuButtons">
-
-            <Route render={({ history}) => (
-              <button type="button" onClick={() => { history.push('/menu') }}>
-                Homepage
+        <h3>
+          This app is a tool to help aid Karolinska Institutet<br></br>
+          in their study about sleep patterns and its effects, <br></br>by
+          gathering data from study participants.<br></br>
+          <br></br>
+          Your participation will contribute to<br></br>
+          researchers gaining more knowledge about<br></br>
+          sleep patterns, and in turn becoming more able<br></br>
+          to help people live more productive and fulfilling lives.
+        </h3>
+        <div className="menuButtons">
+          <Route
+            render={({ history }) => (
+              <button
+                type="button"
+                onClick={() => {
+                  history.push("/menu");
+                }}
+              >
+                Menu
               </button>
-            )} />
-              </div>
+            )}
+          />
         </div>
-      );
-    }
+      </div>
+    );
   }
+}
 
 export default About;
--- src/components/loginReg.js/auth.js
+++ src/components/loginReg.js/auth.js
@@ -1,74 +1,65 @@
 import React, { useState } from "react";
-import {Modal,Button} from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import { Modal, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.css";
 
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-const Auth = () =>  {
-    const[email, setEmail] = useState('');
-    const[password, setPassword] = useState('');
-    
+  const onChange = (event) => {
+    if (event.target.name == "email") {
+      setEmail(event.target.value);
+    } else setPassword(event.target.value);
+  };
 
-   const onChange = (event) =>  {
-       if(event.target.name == 'email'){
-        setEmail(event.target.value);
-       }
-       else
-        setPassword(event.target.value);
-    }
+  return (
+    <div id="flexgrid">
+      <h1>Welcome to the sleeping study</h1>
 
-    
-      return (
-        <div id="flexgrid">
-            <h1>Welcome to the sleeping study</h1>
-
-            <br/>
-            <br/>
-            <br/>
-            <p>
-                Your participation will help
-                understand human sleep behavior.
-            </p>
-            <br />
-        <div className="Formholder">
+      <br />
+      <br />
+      <br />
+      <p>Your participation will help understand human sleep behavior.</p>
+      <br />
+      <div className="Formholder">
         <span id="logintext">Log in</span>
         <form>
-            <input 
-                name="email"
-                className="logininput"
-                value={email}
-                onChange={onChange}
-                type="text"
-                placeholder="Email Address"
-                autoComplete="username"
-            />
-            <br/>
-            <input 
-                name="password"
-                className="logininput"
-                value={password}
-                onChange={onChange}
-                type="password"
-                placeholder="Password"
-                autoComplete="current-password"
-            />
-            <br />
-            <br />
-            <Button >
-                Sign in
-            </Button>
-            <br/>
-            <br/>
+          <input
+            name="email"
+            className="logininput"
+            value={email}
+            onChange={onChange}
+            type="text"
+            placeholder="Email Address"
+            autoComplete="username"
+          />
+          <br />
+          <input
+            name="password"
+            className="logininput"
+            value={password}
+            onChange={onChange}
+            type="password"
+            placeholder="Password"
+            autoComplete="current-password"
+          />
+          <br />
+          <br />
+          <Button>Sign in</Button>
+          <br />
+          <Link to="/menu">
+            <Button>Log in Anonymously</Button>
+          </Link>
+          <br />
+          <br />
         </form>
+      </div>
 
-        </div>
+      <div id="messageModal"></div>
+    </div>
+  );
+};
 
-        <div id="messageModal">
-            
-        </div>
-
-        </div>
-      );
-  }
- 
 export default Auth;
