c0ab164574366dd28863a3268ef694611bf00f91
jabir <jabir@kth.se>
2021-02-05 19:24:40 +0100

    knapp funktionalitet i questionnaire

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 
 import { Link } from "react-router-dom";
-import { Route } from "react-router-dom";
 import "./css/menu.css";
 
 const Menu = () => {
