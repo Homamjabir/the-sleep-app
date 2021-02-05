e37708597937d9a1d1952ad434ee0e25ec710396
TheSnowballz <46904975+TheSnowballz@users.noreply.github.com>
2021-02-05 19:21:04 +0100

    blabla

--- src/components/about.js
+++ src/components/about.js
@@ -20,34 +20,21 @@ class About extends Component {
                  sleep patterns, and in turn becoming more able<br></br>
                   to help people live more productive and fulfilling lives.
             </p>
-            <div className = "menuButtons">
 
-      <h3>
-        This app is a tool to help aid Karolinska Institutet<br></br>
-        in their study about sleep patterns and its effects, <br></br>by
-        gathering data from study participants.<br></br>
-        <br></br>
-        Your participation will contribute to<br></br>
-        researchers gaining more knowledge about<br></br>
-        sleep patterns, and in turn becoming more able<br></br>
-        to help people live more productive and fulfilling lives.
-      </h3>
       <div className="menuButtons">
         <Route
           render={({ history }) => (
             <button
-              type="button"
-              onClick={() => {
-                history.push("/menu");
-              }}
+              type="button" onClick={() => {
+history.push("/menu");}}
             >
               Menu
             </button>
-          )}
-        />
+          )} />
       </div>
     </div>
+
   );
-};
+}
 
 export default About;
