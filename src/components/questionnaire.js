ffdaeea25a982b7338710906cfb239371afc0d20
jabir <jabir@kth.se>
2021-02-05 16:35:31 +0100

    added questionnaire page

--- src/components/questionnaire.js
+++ src/components/questionnaire.js
@@ -1,10 +1,176 @@
 import React, { Component } from "react";
+import "./css/questionnaire.css"
 
 class Questionnaire extends Component {
     render() {
       return (
         <div className = "questionnaireContainer">
-          Questionnaire
+          <h1>
+            Questionnaire
+          </h1>
+          <h2>
+            Hello there, human
+            <br/>
+            I got some questions for you
+          </h2>
+
+          <div className = "questions">
+
+            <div className = "question 1">
+              <p1>
+                Question 1:
+                <br/>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p1>
+              <div>
+                <input type="radio"  /> Integer eros tortor
+                <br/>
+                <input type="radio" /> Integer eros tortor
+                <br/>
+                <input type="radio" /> Integer eros tortor
+              </div>
+            </div>
+            <br/>
+            <div className = "question 2">
+              <p1>
+                Question 2:
+                <br/>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p1>
+              <div>
+                <input type="radio" /> Integer eros tortor
+                <br/>
+                <input type="radio" /> Integer eros tortor
+                <br/>
+                <input type="radio"  /> Integer eros tortor
+              </div>
+            </div>
+            <br/>
+            <div>
+              <p1>
+                Question 3:
+                <br/>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p1>
+              <div>
+                <input type="radio"  /> Integer eros tortor
+                <br/>
+                <input type="radio"  /> Integer eros tortor
+                <br/>
+                <input type="radio"  /> Integer eros tortor
+              </div>
+            </div>
+            <br/>
+            <div>
+              <p1>
+                Question 4:
+                <br/>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p1>
+              <div>
+                <input type="radio" /> Integer eros tortor
+                <br/>
+                <input type="radio"  /> Integer eros tortor
+                <br/>
+                <input type="radio" /> Integer eros tortor
+              </div>
+            </div>
+            <br/>
+            <div>
+              <p1>
+                Question 5:
+                <br/>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p1>
+              <div>
+                <input type="radio" /> Integer eros tortor
+                <br/>
+                <input type="radio"  /> Integer eros tortor
+                <br/>
+                <input type="radio"  /> Integer eros tortor
+              </div>
+            </div>
+            <br/>
+            <div>
+              <p1>
+                Question 6:
+                <br/>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p1>
+              <div>
+                <input type="radio" /> Integer eros tortor
+                <br/>
+                <input type="radio"  /> Integer eros tortor
+                <br/>
+                <input type="radio" /> Integer eros tortor
+              </div>
+            </div>
+            <br/>
+            <div>
+              <p1>
+                Question 7:
+                <br/>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p1>
+              <div>
+                <input type="radio"  /> Integer eros tortor
+                <br/>
+                <input type="radio" /> Integer eros tortor
+                <br/>
+                <input type="radio" /> Integer eros tortor
+              </div>
+            </div>
+            <br/>
+            <div>
+              <p1>
+                Question 8:
+                <br/>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p1>
+              <div>
+                <input type="radio" /> Integer eros tortor
+                <br/>
+                <input type="radio" /> Integer eros tortor
+                <br/>
+                <input type="radio" /> Integer eros tortor
+              </div>
+            </div>
+            <br/>
+            <div>
+              <p1>
+                Question 9:
+                <br/>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p1>
+              <div>
+                <input type="radio" /> Integer eros tortor
+                <br/>
+                <input type="radio"/> Integer eros tortor
+                <br/>
+                <input type="radio" /> Integer eros tortor
+              </div>
+            </div>
+            <br/>
+            <div>
+              <p1>
+                Question 10:
+                <br/>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p1>
+              <div>
+                <input type="radio" /> Integer eros tortor
+                <br/>
+                <input type="radio"  /> Integer eros tortor
+                <br/>
+                <input type="radio"  /> Integer eros tortor
+              </div>
+            </div>
+          </div>
+          <div>
+            <button className = "buttonTest">
+                Done
+            </button>
+          </div>
         </div>
       );
     }
