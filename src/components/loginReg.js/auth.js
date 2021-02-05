79ecab0b8e6ec147a25310fab9e7847e66cf2e1d
aroush <aroush@kth.se>
2021-02-05 13:40:35 +0100

    Installed bootstrap, updated login screen

--- src/components/loginReg.js/auth.js
+++ src/components/loginReg.js/auth.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import {Modal,Button} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 class Auth extends Component {
 
@@ -12,17 +15,46 @@ class Auth extends Component {
             showMessage: false,
             showForgot: false,
         };
+    }
 
-
+    onChange = event => {
+        this.setState({[event.target.name]: event.target.value});
     }
 
     render() {
+        const {email,password} = this.state;
       return (
         <div id="flexgrid">
             
         <div className="Formholder">
         <span id="logintext">Log in</span>
-        
+        <form>
+            <input 
+                name="email"
+                className="logininput"
+                value={email}
+                onChange={this.onChange}
+                type="text"
+                placeholder="Email Address"
+                autoComplete="username"
+            />
+            <br/>
+            <input 
+                name="password"
+                className="logininput"
+                value={password}
+                onChange={this.onChange}
+                type="password"
+                placeholder="Password"
+                autoComplete="current-password"
+            />
+            <br/>
+            <Button onClick={this.Signin}>
+                Sing in
+            </Button>
+            <br/>
+            <br/>
+        </form>
 
         </div>
 
