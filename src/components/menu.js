3e075f14c84011ec8624c34b16ccb38a03c67209
Alexander Palm <alepa@kth.se>
2021-02-05 16:43:05 +0100

    Lyckades få in en timer som räknar ner tills nästa test

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,39 +1,109 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import { Route } from "react-router-dom";
-import "./css/menu.css"
+import "./css/menu.css";
 
-class Menu extends Component {
-    render() {
-      return (
-        <div className = "menuContainer">
-            Main Menu
+const Menu = () => {
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
 
-            <div className = "menuButtons">
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+  });
 
-              <Route render={({ history}) => (
-                <button type="button" onClick={() => { history.push('/questionnaire') }}>
-                  Questionnaire
-                </button>
-              )} />  
-              <Route render={({ history}) => (
-                <button type="button" onClick={() => { history.push('/mini-games') }}>
-                  Mini-games
-                </button>
-              )} />  
-              <Route render={({ history}) => (
-                <button type="button" onClick={() => { history.push('/statistics') }}>
-                  Statistics
-                </button>
-              )} />  
-              <Route render={({ history}) => (
-                <button type="button" onClick={() => { history.push('/settings') }}>
-                  Settings
-                </button>
-              )} />  
-            </div>
-        </div>
-      );
+  const timerComponents = [];
+
+  Object.keys(timeLeft).forEach((interval, index) => {
+    if (!timeLeft[interval]) {
+      return;
     }
+
+    timerComponents.push(
+      <span>
+        {timeLeft[interval]}
+        {index !== 2 ? ":" : ""}
+      </span>
+    );
+  });
+
+  return (
+    <div className="menuContainer">
+      <h3>Main Menu</h3>
+      <div>
+        {timerComponents.length ? (
+          timerComponents
+        ) : (
+          <span>Time for a new test!</span>
+        )}
+      </div>
+      <div className="menuButtons">
+        <Route
+          render={({ history }) => (
+            <button
+              type="button"
+              onClick={() => {
+                history.push("/questionnaire");
+              }}
+            >
+              Questionnaire
+            </button>
+          )}
+        />
+        <Route
+          render={({ history }) => (
+            <button
+              type="button"
+              onClick={() => {
+                history.push("/mini-games");
+              }}
+            >
+              Mini-games
+            </button>
+          )}
+        />
+        <Route
+          render={({ history }) => (
+            <button
+              type="button"
+              onClick={() => {
+                history.push("/statistics");
+              }}
+            >
+              Statistics
+            </button>
+          )}
+        />
+        <Route
+          render={({ history }) => (
+            <button
+              type="button"
+              onClick={() => {
+                history.push("/settings");
+              }}
+            >
+              Settings
+            </button>
+          )}
+        />
+      </div>
+    </div>
+  );
+};
+
+const calculateTimeLeft = () => {
+  let time = new Date(JSON.parse(localStorage.getItem("startTime")));
+  let difference = time - new Date();
+
+  let timeLeft = {};
+  if (difference > 0) {
+    timeLeft = {
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / 1000 / 60) % 60),
+      seconds: Math.floor((difference / 1000) % 60),
+    };
   }
- 
+
+  return timeLeft;
+};
+
 export default Menu;
--- src/components/reactionGameResults.js
+++ src/components/reactionGameResults.js
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const ReactionGameResults = () => {
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+  });
+
+  const timerComponents = [];
+
+  Object.keys(timeLeft).forEach((interval, index) => {
+    if (!timeLeft[interval]) {
+      return;
+    }
+
+    timerComponents.push(
+      <span>
+        {timeLeft[interval]}
+        {index !== 2 ? ":" : ""}
+      </span>
+    );
+  });
+
   return (
     <div className="reaction-game-result-wrapper">
       <h3>Your speed was {localStorage.getItem("reactionTime")} seconds</h3>
       <div className="reaction-game-info-text">
         <p>Check back in 4 hours for another test</p>
         <br />
-        <p>03:59:59</p>
+        <div>
+          {timerComponents.length ? (
+            timerComponents
+          ) : (
+            <span>Time for a new test!</span>
+          )}
+        </div>
       </div>
 
       <Link to="/menu">
@@ -16,6 +45,24 @@ const ReactionGameResults = () => {
       </Link>
     </div>
   );
+};
+
+const calculateTimeLeft = () => {
+  let time = new Date();
+  time.setHours(time.getHours() + 3, 59, 59);
+  localStorage.setItem("startTime", JSON.stringify(time));
+  let difference = time - new Date();
+
+  let timeLeft = {};
+  if (difference > 0) {
+    timeLeft = {
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / 1000 / 60) % 60),
+      seconds: Math.floor((difference / 1000) % 60),
+    };
+  }
+
+  return timeLeft;
 };
 
 export default ReactionGameResults;
