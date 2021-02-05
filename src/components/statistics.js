ab59cbe5da0318652c4d1bac2768b479c7e35dba
jabir <jabir@kth.se>
2021-02-05 22:33:47 +0100

    dfgdfg

--- src/components/statistics.js
+++ src/components/statistics.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import ReturnIcon from "../images/return-icon.jpg";
+import "./css/statistics.css"
 
 const Statistics = () => {
   return (
@@ -8,7 +9,7 @@ const Statistics = () => {
       <Link to="/menu" className="returnIcon">
         <img src={ReturnIcon} alt="test" width="20" height="20"></img>
       </Link>
-      <p>Your statistics compared to other participants</p>
+      <h3>Your statistics compared to other participants</h3>
     </div>
   );
 };
