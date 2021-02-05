99b0f9c7b6a123226b38dfdd7433108326a5be8a
aroush <aroush@kth.se>
2021-02-05 11:53:46 +0100

    Updated packages and installed firebsae

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { firebaseConfig } from "./configs/configFirebase";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore"
+
+
+function initFirebase () {
+firebase.initializeApp(firebaseConfig);
+}
+initFirebase();
+
 
 ReactDOM.render(
   <React.StrictMode>
