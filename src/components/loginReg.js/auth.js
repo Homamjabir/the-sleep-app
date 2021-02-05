cafeb9520786b49cce46dd884987ccb4395d323c
aroush <aroush@kth.se>
2021-02-05 21:07:32 +0100

    Fixed auth.js

--- src/components/loginReg.js/auth.js
+++ src/components/loginReg.js/auth.js
@@ -1,65 +1,82 @@
 import React, { useState } from "react";
-import { Modal, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/css/bootstrap.css";
+import {Modal,Button} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
-const Auth = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
 
-  const onChange = (event) => {
-    if (event.target.name == "email") {
-      setEmail(event.target.value);
-    } else setPassword(event.target.value);
-  };
+const Auth = () =>  {
+    const[email, setEmail] = useState('');
+    const[password, setPassword] = useState('');
+    const[showMessage,setShowMessage] = useState(false);
 
-  return (
-    <div id="flexgrid">
-      <h1>Welcome to the sleeping study</h1>
+   const onChange = (event) =>  {
+       if(event.target.name == 'email'){
+        setEmail(event.target.value);
+       }
+       else
+        setPassword(event.target.value);
+    }
 
-      <br />
-      <br />
-      <br />
-      <p>Your participation will help understand human sleep behavior.</p>
-      <br />
-      <div className="Formholder">
+    const hideMessage = () => {
+        setShowMessage(false);
+    }
+    
+      return (
+        <div id="flexgrid">
+            <h1>Welcome to the sleeping study</h1>
+
+            <br/>
+            <br/>
+            <br/>
+            <p>
+                Your participation will help
+                understand human sleep behavior.
+            </p>
+            <br />
+        <div className="Formholder">
         <span id="logintext">Log in</span>
         <form>
-          <input
-            name="email"
-            className="logininput"
-            value={email}
-            onChange={onChange}
-            type="text"
-            placeholder="Email Address"
-            autoComplete="username"
-          />
-          <br />
-          <input
-            name="password"
-            className="logininput"
-            value={password}
-            onChange={onChange}
-            type="password"
-            placeholder="Password"
-            autoComplete="current-password"
-          />
-          <br />
-          <br />
-          <Button>Sign in</Button>
-          <br />
-          <Link to="/menu">
+            <input 
+                name="email"
+                className="logininput"
+                value={email}
+                onChange={onChange}
+                type="text"
+                placeholder="Email Address"
+                autoComplete="username"
+            />
+            <br/>
+            <input 
+                name="password"
+                className="logininput"
+                value={password}
+                onChange={onChange}
+                type="password"
+                placeholder="Password"
+                autoComplete="current-password"
+            />
+            <br />
+            <br />
+            <Link to="/menu">
             <Button>Log in Anonymously</Button>
-          </Link>
-          <br />
-          <br />
+            </Link>
+            <br/>
+            <br/>
         </form>
-      </div>
 
-      <div id="messageModal"></div>
-    </div>
-  );
-};
+        </div>
 
+        <div id="messageModal">
+            <Modal aria-labelledby="contained-modal-title-vcenter" centered
+            show={showMessage} onHide={hideMessage}>
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter" className="titles">{message}</Modal.Title>
+                </Modal.Header>
+            </Modal>
+        </div>
+
+        </div>
+      );
+  }
+ 
 export default Auth;
